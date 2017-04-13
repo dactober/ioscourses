967570d4ef2055ec9151d750ad71e75d62447a82
Aleksey Drachyov <intego111@gmail.com>
2017-04-14 00:27:15 +0300

    Added ScrollView

---
+++ sprint01/sprint01/CellDataModel.h
@@ -0,0 +1,19 @@
+//
+//  CellDataModel.h
+//  sprint01
+//
+//  Created by Aleksey Drachyov on 4/13/17.
+//  Copyright (c) 2017 alekseydrachyov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface CellDataModel : NSManagedObject
+
+@property (nonatomic, retain) NSString * image;
+@property (nonatomic, retain) NSString * subtitle;
+@property (nonatomic, retain) NSString * title;
+
+@end
---
+++ sprint01/sprint01/CellDataModel.m
@@ -0,0 +1,18 @@
+//
+//  CellDataModel.m
+//  sprint01
+//
+//  Created by Aleksey Drachyov on 4/13/17.
+//  Copyright (c) 2017 alekseydrachyov. All rights reserved.
+//
+
+#import "CellDataModel.h"
+
+
+@implementation CellDataModel
+
+@dynamic image;
+@dynamic subtitle;
+@dynamic title;
+
+@end
